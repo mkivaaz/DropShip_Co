dc9aadfb680a5f2948f12cc9822ff4dcfb9447f4
mkivaaz@gmail.com <X45314523>
2018-06-23 23:50:50 +0800

    Added Navigation Drawer

---
+++ app/src/main/java/org/com/dropship_co/Data/Category.java
@@ -0,0 +1,17 @@
+package org.com.dropship_co.Data;
+
+public class Category {
+    String categoryName;
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
